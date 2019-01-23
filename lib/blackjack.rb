8fa275599533c63b9276c6c7666f294cb609b282
Alonso Ortega <alonsojortega@gmail.com>
2019-01-23 21:42:06 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,33 +1,38 @@
-def welcome
-  puts "Welcome to the Blackjack Table"
-end
-
 def deal_card
   rand(1..11)
 end
 
 def display_card_total(card_total)
-  puts "Your total is #{card_total}"
+  puts "Your cards add up to #{card_total}"
 end
+
+
+##########
+# rounds #
+##########
+
+def welcome
+  puts "Welcome to the Blackjack Table"
+end
+
 
 def prompt_user
   puts "Type 'h' to hit or 's' to stay"
+end
+
+
+def initial_round
+  first_round = deal_card + deal_card
+  display_card_total(first_round)
+  return first_round
 end
 
 def get_user_input
   gets.chomp.strip
 end
 
-def end_game
+def end_game(card_total)
   puts "Sorry, you hit #{card_total}. Thanks for playing!"
-end
-
-def initial_round
-  card1 = deal_card
-  card2 = deal_card
-  total = card1 + card2
-  display_card_total(total)
-  return total
 end
 
 def hit?(card_total)
@@ -38,7 +43,6 @@ def hit?(card_total)
     prompt_user
     input = get_user_input
   end
-
   if input == 'h'
     card_total += deal_card
   elsif input == 's'
@@ -46,13 +50,14 @@ def hit?(card_total)
   end
 end
 
+
 def invalid_command
   puts "Please enter a valid command"
 end
 
-#####################################################
-# get every test to pass before coding runner below #
-#####################################################
+##########
+# runner #
+##########
 
 def runner
   welcome
@@ -61,6 +66,5 @@ def runner
     card_total = hit?(card_total)
     display_card_total(card_total)
   end
-
   end_game(card_total)
 end
